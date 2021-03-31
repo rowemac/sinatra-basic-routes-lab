0358f800eeefec426b52bd8d1a374dcea4682d46
Mac Rowe <m.rowe42@gmail.com>
2021-03-31 09:22:48 -0400

    commit tree 9f54e197017dd0002b241d1f71b25cd7d1fc549b

--- app.rb
+++ app.rb
@@ -1,4 +1,19 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+    get '/name' do 
+        "My name is Mac"
+    end 
+
+    get '/hometown' do 
+        "My hometown is Richmond, VA"
+    end 
+
+    get '/favorite-song' do 
+        "My favorite song is I could never pick."
+    end 
+
+
+
 end
